0a103c5296be2d47ed8a5de94d7ec1854f7da25c
flyside <flyside@bk.ru>
2019-08-16 09:55:45 +0300

    add article category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ActiveRecord::Base
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
