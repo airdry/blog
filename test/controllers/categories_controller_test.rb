7d479672ea49637dc8a068c711b8d1b17c0d40ca
flyside <flyside@bk.ru>
2019-08-16 09:31:48 +0300

    add admin requirement ro create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,21 +1,31 @@
 require "test_helper"
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "sports")
+    @category = Category.create(name: "Sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
-  test "should get category index" do
+  test "should get categories index" do
     get categories_path
     assert_response :success
   end
 
   test "should get new" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
+
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "sports" } }
+    end
+    assert_redirected_to categories_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  def sign_in_as(user, password)
+    post login_path, params: { session: { email: user.email, password: password } }
+  end
 
   # Add more helper methods to be used by all tests here...
 end
