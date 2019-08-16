5cbd1ec3cad38845582c80b0a2816b662e5e3677
flyside <flyside@bk.ru>
2019-08-16 10:08:00 +0300

    update views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,11 +50,11 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
-    if current_user != @article.user and !current_user.admin?
+    if current_user != @article.user && !current_user.admin?
       flash[:danger] = "You can only edit or delete your own article"
       redirect_to root_path
     end
