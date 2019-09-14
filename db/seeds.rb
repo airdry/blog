6f19b98a12047ca5bf342d37356c6e8b6327d487
flyside <flyside@bk.ru>
2019-09-14 09:13:02 +0300

    fix: edit database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'admin', password: '123456789', email: 'admin@mail.com', admin: 'true')
+User.create(username: 'user', password: '123456789', email: 'user@mail.com', admin: 'false')
+User.create(username: 'current', password: '123456789', email: 'current@mail.com', admin: 'false')
