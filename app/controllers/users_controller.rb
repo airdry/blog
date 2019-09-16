cecce9b8dc00b22bb177d30411ef2d171afa0165
flyside <flyside@bk.ru>
2019-09-16 10:04:19 +0300

    fix: change blog platform

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      flash[:success] = "Welcome to the Blog #{@user.username}"
       redirect_to user_path(@user)
     else
       render 'new'
