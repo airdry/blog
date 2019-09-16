d6b5c2064bbbf581e47833ad619995c016c10b80
flyside <flyside@bk.ru>
2019-09-16 11:14:59 +0300

    fix: add validate for show users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
   before_action :require_same_user, only: %i[edit update destroy]
   before_action :require_admin, only: [:destroy]
+  before_action :require_user
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
